41b62250f094658940b6723107eeeb4e962d61d5
carib4ik <carib4ik@gmail.com>
2025-08-04 22:02:29 +0800

    update greetings message

--- VladTelegramBot/Data/GlobalData.cs
+++ VladTelegramBot/Data/GlobalData.cs
@@ -8,7 +8,7 @@ public static class GlobalData
     public const string Excel = "RequestExcelFile";
     
     public const string GreetingsAfterTest = "Ты успешно прошёл опрос — спасибо!";
-    public const string GreetingsBeforeTest = "Приветствую! \nЭто бот, который даёт доступ к моему Telegram-каналу про бизнес-опыт.\n\nЯ не учу. Я делюсь тем, что сам прохожу:\nошибки, обнуления, рост, масштабирование, инвестиции, цифры.\nВсё, что работает — и всё, что не сработало.\n\nПеред входом — 5 коротких анонимных вопросов.\nХочу понимать свою аудиторию и собирать сильное окружение.";
+    public const string GreetingsBeforeTest = "Перед входом — 5 коротких анонимных вопросов.\nХочу понимать свою аудиторию и собирать сильное окружение.";
     
     public const string Question1 = "Какой у тебя сейчас бизнес (или ниша) / должность (если найм)?";
     public const string Question2 = "Личный доход в месяц (в $)?";
